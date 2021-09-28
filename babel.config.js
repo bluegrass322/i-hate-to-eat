0f15d8341c0a88953733b880345e95a2ed4bfe4e
sou7322 <skoike322@gmail.com>
2021-09-28 20:02:57 +0900

    [Add] Babelの設定を追記
    Herokuデプロイ時に出たwarnの内容に従って、Babelの設定ファイルに
    `loose: true`オプションを追記。

--- babel.config.js
+++ babel.config.js
@@ -48,6 +48,24 @@ module.exports = function(api) {
         }
       ],
       [
+        "@babel/plugin-proposal-private-property-in-object",
+        {
+          loose: true
+        }
+      ],
+      [
+        "@babel/plugin-proposal-private-methods",
+        {
+          loose: true
+        }
+      ],
+      [
+        "@babel/plugin-proposal-private-property-in-object",
+        {
+          loose: true
+        }
+      ],
+      [
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
