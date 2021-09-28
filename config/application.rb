77181182f4f46f0710c6e892f0fcff751df9d5cf
sou7322 <skoike322@gmail.com>
2021-09-28 19:23:53 +0900

    [Add] ロケールの設定を追記

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module IHateToEat
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
