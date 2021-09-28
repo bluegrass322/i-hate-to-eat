424bdf4d145acc1644b817fac6b492d93c7a102c
sou7322 <skoike322@gmail.com>
2021-09-28 20:15:00 +0900

    [Add] Babelの設定ファイルで記述の重複を修正

--- babel.config.js
+++ babel.config.js
@@ -60,12 +60,6 @@ module.exports = function(api) {
         }
       ],
       [
-        "@babel/plugin-proposal-private-property-in-object",
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
