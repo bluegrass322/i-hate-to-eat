da3104f06bf56a3591147de0521b522c265ae864
sou7322 <skoike322@gmail.com>
2021-09-30 10:19:07 +0900

    [Add] 利用可能なロケールの種類を設定

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module IHateToEat
     config.load_defaults 6.0
 
     config.i18n.default_locale = :ja
+    config.i18n.available_locales = %i[ja]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.time_zone = 'Tokyo'
