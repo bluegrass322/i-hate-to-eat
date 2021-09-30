7fe1df646b4224c96bf05006ca814c4a12e724b2
sou7322 <skoike322@gmail.com>
2021-09-30 17:17:55 +0900

    [Add] Vuetifyの設定ファイルを作成

---
+++ app/javascript/vuetify/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    light: {
+      base: 'F5F5F6',
+      primary: '5A7899',
+      pLight: '89A7CA',
+      pDark: '2C4C6B',
+    },
+  },
+  icons: {
+    iconfont: 'mdi',
+  },
+});
