1283fb4c67ebe16c1bc289ef9390340e863c0d38
sou7322 <skoike322@gmail.com>
2021-10-01 19:47:19 +0900

    [Fix] リントエラーを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 5 }, if: :new_or_changes_password
   validates :password, confirmation: true, if: :new_or_changes_password
   validates :password_confirmation, presence: true, if: :new_or_changes_password
-  
+
   # Instance methods
   private
 
