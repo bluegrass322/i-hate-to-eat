7345d195164de24255d4efa7e95d4680f0766ab6
sou7322 <skoike322@gmail.com>
2021-10-01 13:52:48 +0900

    [Add] Userモデルにenumとバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
+  # Enums
+  enum role: { general: 0, admin: 10 } 
+
+  # Validations
+  with_options presence: true do
+    validates :name, length: { maximum: 50 }
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :role
+  end
+
+  validates :password, length: { minimum: 5 }, if: :new_or_changes_password
+  validates :password, confirmation: true, if: :new_or_changes_password
+  validates :password_confirmation, presence: true, if: :new_or_changes_password
 end
 
 # == Schema Information
