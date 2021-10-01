3d63d0a5ab0169de2a3e743d48a9f11eaff6bb6d
sou7322 <skoike322@gmail.com>
2021-10-01 11:33:08 +0900

    [Add] 仮のヘッダーを作成

---
+++ app/javascript/components/commons/CommonHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    flat
+    fixed
+    height="70"
+  >
+    <span class="text-h5 base--text">
+      I hate to Eat
+    </span>
+    <!-- <v-card
+      flat
+      width="300px"
+      height="50px"
+    >
+      <span class="text-center text-h3">
+        Ad
+      </span>
+    </v-card> -->
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
