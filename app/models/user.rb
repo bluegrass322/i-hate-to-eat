3a7cb2b4bb44c3133116c750d888736de8948226
sou7322 <skoike322@gmail.com>
2021-10-01 18:21:35 +0900

    [Add] userモデルファイルにインスタンスメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,13 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 5 }, if: :new_or_changes_password
   validates :password, confirmation: true, if: :new_or_changes_password
   validates :password_confirmation, presence: true, if: :new_or_changes_password
+  
+  # Instance methods
+  private
+
+    def new_or_changes_password
+      new_record? || changes[:crypted_password]
+    end
 end
 
 # == Schema Information
