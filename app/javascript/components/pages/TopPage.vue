d8ba0cced8fad90ef5aba873e4a9f6ca7d7ab256
sou7322 <skoike322@gmail.com>
2021-10-01 11:34:43 +0900

    [Add] 仮のトップページを作成

--- app/javascript/components/pages/TopPage.vue
+++ app/javascript/components/pages/TopPage.vue
@@ -1,7 +1,55 @@
 <template>
-  <p>Top page</p>
+  <v-container class="container">
+    <div class="date">
+      <p class="date-text text-h5 primary--text">
+        {{ date }}
+      </p>
+    </div>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+export default {
+  data() {
+    return {
+      date: '',
+    }
+  },
+  mounted() {
+    this.setDate();
+  },
+  methods: {
+    setDate() {
+      dayjs.extend(utc);
+      dayjs.extend(timezone);
+
+      const today = dayjs();
+      this.date = today.tz("Asia/Tokyo").format('YYYY.MM.DD')
+  }
+  }
+};
 </script>
+
+<style scoped>
+.container {
+  margin: 10px, 0;
+  padding: 0;
+}
+
+.date {
+  margin-top: 20px;
+  height: 30;
+  border-bottom: medium solid #5A7899;
+  /* エリア確認用 */
+  /* background-color: rgba(90, 40, 80, 0.3); */
+}
+
+.date-text {
+  margin: 0 20px;
+  padding: 0;
+}
+</style>
