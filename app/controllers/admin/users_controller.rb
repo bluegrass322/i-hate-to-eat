351756deb940cb55ad177e9f9b4a7f4dce9503c8
sou7322 <skoike322@gmail.com>
2021-10-03 11:36:42 +0900

    [Add] admin/usersコントローラーにshowアクションを追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,15 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
     @users = User.all.order(created_at: "DESC")
   end
+
+  def show; end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
