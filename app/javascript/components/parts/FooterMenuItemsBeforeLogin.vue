f47e1d83b122c5dff5af47c2e4483d7131d74202
sou7322 <skoike322@gmail.com>
2021-10-03 20:34:55 +0900

    [Fix] フッターメニューの体裁を少々修正

--- app/javascript/components/parts/FooterMenuItemsBeforeLogin.vue
+++ app/javascript/components/parts/FooterMenuItemsBeforeLogin.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <v-btn to="/" color="base" text min-height="56" width="56"> top </v-btn>
-    <v-btn to="/register" color="base" text min-height="56"> register </v-btn>
+    <v-btn to="/" color="base" text tile min-height="56" width="56" class="ma-0"
+      >top</v-btn
+    >
+    <v-btn
+      to="/register"
+      color="base"
+      text
+      tile
+      min-height="56"
+      width="150"
+      class="ma-0"
+      >register</v-btn
+    >
   </div>
 </template>
 
