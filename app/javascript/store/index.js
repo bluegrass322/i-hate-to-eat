bd3408f263f8728c3d5436e2fa3f33af9c800647
sou7322 <skoike322@gmail.com>
2021-10-03 18:39:23 +0900

    [Add] フラッシュメッセージ用のストアをindexで読み込み

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import flashMessage from './modules/flashMessage';
+
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
+    flashMessage
   }
 });
 
