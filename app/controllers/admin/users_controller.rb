0bb8a06409aa0d0e3d2eb7c3da82a0dc2aba01bd
sou7322 <skoike322@gmail.com>
2021-10-03 15:25:17 +0900

    [Add] admin/usersコントローラーにdestroyアクションを追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,11 +11,16 @@ class Admin::UsersController < Admin::BaseController
   
   def update
     if @user.update(user_params)
-      redirect_to admin_user_path(@user), success: "ユーザー情報の更新に成功"
+      redirect_to admin_user_path(@user), notice: "ユーザー情報の更新に成功"
     else
       flash.now[:danger] = "ユーザーの更新に失敗"
       render :edit
     end
+  end
+
+  def destroy
+    @user.destroy!
+    redirect_to admin_users_path, notice: "ユーザーの削除に成功"
   end
 
   private
