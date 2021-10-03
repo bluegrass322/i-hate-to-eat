022fc0432ceb68b05ab84decc2a047a87645d86e
sou7322 <skoike322@gmail.com>
2021-10-03 23:09:35 +0900

    [Add] create destroyアクションを追加

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -3,6 +3,20 @@
 module Api
   module V1
     class AuthenticationsController < ApplicationController
+      def create
+        @user = login(params[:email], params[:password])
+
+        if @user
+          render json { id: @user.id }
+        else
+          render400(nil, "ログインに失敗しました")
+        end
+      end
+
+      def destroy
+        logout
+        head :ok
+      end
     end
   end
 end
