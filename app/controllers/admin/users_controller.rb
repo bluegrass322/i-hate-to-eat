192e8689fa229947e1e5650916a65517d3b46156
sou7322 <skoike322@gmail.com>
2021-10-03 11:38:56 +0900

    [Add] admin/usersコントローラーにedit updateアクションを追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,9 +7,25 @@ class Admin::UsersController < Admin::BaseController
 
   def show; end
 
+  def edit; end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), success: "ユーザー情報の更新に成功"
+    else
+      flash.now[:danger] = "ユーザーの更新に失敗"
+      render :edit
+    end
+  end
+
   private
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :pasword_confirmation,
+                                   :role)
     end
 end
