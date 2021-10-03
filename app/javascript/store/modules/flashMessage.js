7cee7f514bd44931c885069540ad10403154edf9
sou7322 <skoike322@gmail.com>
2021-10-03 18:38:44 +0900

    [Add] フラッシュメッセージ用のストアを作成

---
+++ app/javascript/store/modules/flashMessage.js
@@ -0,0 +1,33 @@
+const state = {
+  type: 'success',
+  message: ''
+};
+
+const getters = {
+  type: state => state.type,
+  message: state => state.message
+};
+
+const mutations = {
+  setMessage (state, { type, message, timeout }) {
+    state.type = type;
+    state.message = message;
+
+    if (typeof timeout === 'undefined') {
+      timeout = 3000;
+    };
+
+    setTimeout(() => {
+      state.type = 'success';
+      state.message = '';
+    },
+    timeout);
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations
+};
