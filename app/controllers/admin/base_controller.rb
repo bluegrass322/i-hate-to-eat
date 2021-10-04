fe8bd868a2fd63e7b308e72019620ce125d8b77e
sou7322 <skoike322@gmail.com>
2021-10-04 16:46:50 +0900

    [Add] 管理画面用のbaseコントローラーにコールバックを追加
    before_actionにログインを強制するメソッドと、管理者権限の有無を
    チェックするメソッドを設定。

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,8 +2,18 @@
 
 module Admin
   class BaseController < ApplicationController
-    before_action :require_login
+    before_action :require_login, :check_admin
 
     layout 'admin/layouts/application'
+
+    private
+      def not_authenticated
+        flash[:danger] = "ログインしてください"
+        redirect_to admin_login_path
+      end
+
+      def check_admin
+        redirect_to(root_path) unless current_user.admin?
+      end
   end
 end
