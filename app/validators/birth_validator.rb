c422bb68a628cefaac77879e1ac3000fbc694c03
sou7322 <skoike322@gmail.com>
2021-10-04 18:47:53 +0900

    [Add] birthカラム用のカスタムバリデータを作成
    入力された生年月日が未来のものでないか、また生年月日から計算される
    現在の年齢が18歳以上60歳未満であるかをチェックするバリデーションを作成。

---
+++ app/validators/birth_validator.rb
@@ -0,0 +1,10 @@
+class BirthValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return unless value
+
+    record.errors.add(:birth, "未来の日付は入力できません") if value.future?
+
+    age = record.calc_age
+    record.errors.add(:birth, "本サービスは18歳以上60歳未満の方が対象です") if age < 18 || age >= 60
+  end
+end
