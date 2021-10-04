a92e8b9a9a83ceb5357a8a7c6b0c828570c94019
sou7322 <skoike322@gmail.com>
2021-10-04 15:01:45 +0900

    [Add] フロントのルーティングにアクセス制限を実装
    vue-routerのルートメタっフィールドを利用して、ログインの有無によって
    ページへのアクセス制限をかける処理を追加。

--- app/javascript/router/router.js
+++ app/javascript/router/router.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '../store/index';
 
 import TopPage from '../components/pages/TopPage';
 import RegisterPage from '../components/pages/RegisterPage';
 import LoginPage from '../components/pages/LoginPage';
+import HomePage from '../components/pages/HomePage';
 import NotFound from '../components/pages/NotFound';
 
 Vue.use(VueRouter);
@@ -15,25 +17,53 @@ const router = new VueRouter({
       path: '/',
       component: TopPage,
       name: 'TopPage',
+      meta: { isPublic: true },
       children: [
         {
           path: 'register',
           component: RegisterPage,
           name: 'RegisterPage',
+          meta: { isPublic: true },
         },
         {
           path: 'login',
           component: LoginPage,
           name: 'LoginPage',
+          meta: { isPublic: true },
         },
       ],
+    },
+    {
+      path: '/home',
+      component: HomePage,
+      name: 'HomePage',
+      meta: { requiresAuth: true },
     },
     // 404 not found
     {
       path: '*',
       component: NotFound,
+      meta: { isPublic: true },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  // isPublicでない=ログインが必要なページの場合
+  if (to.matched.some((record) => !record.meta.isPublic)) {
+    // かつログインしていない場合
+    if (!store.state.authUser.isLoggedIn) {
+      // ログインページにリダイレクトする
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath },
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
