ef4a2814dcd0ccf46e3beb0910b44fa74e9c881b
sou7322 <skoike322@gmail.com>
2021-10-05 17:09:56 +0900

    [Add] ユーザーのDRI設定用コントローラーを作成

---
+++ app/controllers/api/v1/users_dietary_reference_intake_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UsersDietaryReferenceIntakeController < Api::V1::BaseController
+      def show
+      end
+
+      def update
+      end
+    end
+  end
+end
