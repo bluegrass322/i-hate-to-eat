a25e7157529b99bf2e64090f3ce304f33395bd4a
sou7322 <skoike322@gmail.com>
2021-10-06 18:36:09 +0900

    [Add] mypageコントローラーにshowアクションを追加
    これまでbmrコントローラー・users_dietary_reference_intakesコントローラーで
    個別にリクエストを投げて情報を取得していたものを、mypageコントローラーで
    一括取得できるようにアクションを作成。

--- app/controllers/api/v1/mypages_controller.rb
+++ app/controllers/api/v1/mypages_controller.rb
@@ -3,8 +3,20 @@
 module Api
   module V1
     class MypagesController < Api::V1::BaseController
+      before_action :set_user
+
       def show
+        mypage_params = @user.set_mypage_params
+
+        json_string = mypage_params.to_json
+        render json: json_string
       end
+
+      private
+
+        def set_user
+          @user = User.find(current_user.id)
+        end
     end
   end
 end
