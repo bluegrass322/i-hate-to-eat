1934ab55c919b1cbdffe8943e5bdb30b1c6be76b
sou7322 <skoike322@gmail.com>
2021-10-07 16:40:11 +0900

    [Add] Foodモデル管理画面用のコントローラーを作成

---
+++ app/controllers/admin/foods_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Admin
+  class FoodsController < Admin::BaseController
+    def index
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
+end
