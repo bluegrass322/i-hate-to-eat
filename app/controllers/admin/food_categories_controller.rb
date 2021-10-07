f1044b9f84e4e56a86830d92b9ad49f6c0c52ee7
sou7322 <skoike322@gmail.com>
2021-10-07 14:21:56 +0900

    [Add] admin/food_categoriesコントローラーを作成

---
+++ app/controllers/admin/food_categories_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Admin
+  class Admin::FoodCategoriesController < Admin::BaseController
+    def index
+    end
+
+    def edit
+    end
+
+    def update
+    end
+  end
+end
