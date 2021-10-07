9067c704f63e503c1e08422b961dc58a7d0ca40d
sou7322 <skoike322@gmail.com>
2021-10-07 14:32:42 +0900

    [Add] admin/food_categoriesコントローラーにアクションを追加

--- app/controllers/admin/food_categories_controller.rb
+++ app/controllers/admin/food_categories_controller.rb
@@ -2,13 +2,31 @@
 
 module Admin
   class Admin::FoodCategoriesController < Admin::BaseController
+    before_action :set_category, only: %i[edit update]
+
     def index
+      @categories = FoodCategory.all.order(id: "DESC")
     end
 
-    def edit
-    end
+    def edit; end
 
     def update
+      if @category.update(category_params)
+        redirect_to admin_food_categories_path, notice: "食品カテゴリー情報の更新に成功"
+      else
+        flash.now[:danger] = "食品カテゴリーの更新に失敗"
+        render :edit
+      end
     end
+
+    private
+
+      def set_category
+        @category = FoodCategory.find(params[:id])
+      end
+
+      def category_params
+        params.require(:food_category).permit(:name)
+      end
   end
 end
