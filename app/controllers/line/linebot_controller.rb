a083f0d07ddab281772496e63d946568536ae4fd
sou7322 <skoike322@gmail.com>
2021-10-12 21:10:43 +0900

    [Fix] エラーの原因を探るため処理を再度間引き

--- app/controllers/line/linebot_controller.rb
+++ app/controllers/line/linebot_controller.rb
@@ -56,7 +56,7 @@ module Line
       def reply_text_message(event)
         reply_text = case event.message["text"]
                      when "do linking"
-                       set_url_for_linking(event["source"]["userId"])
+                       "userId: #{ event["source"]["userId"] }"
                      when "don't linking"
                        "引き続きLINE通知以外の機能をご利用ください"
                      when "hi"
@@ -74,7 +74,28 @@ module Line
       def set_url_for_linking(line_id)
         # 連携手順1. 連携トークンを発行する
         token = require_link_token(line_id)
-        token
+
+        # 連携手順2. ユーザーを連携URLにリダイレクトする
+        {
+          type: "template",
+          altText: "アカウント連携用ページ",
+          template: {
+            type: "buttons",
+            text: "以下のURLから再度ログインし、アカウント連携を行ってください",
+            defaultAction: {
+              type: "uri",
+              label: "アカウント連携ページ",
+              uri: "https://i-hate-to-eat.herokuapp.com/line/link?linkToken=#{ token['linkToken'] }"
+            },
+            actions: [
+              {
+                type: "uri",
+                label: "アカウント連携ページ",
+                uri: "https://i-hate-to-eat.herokuapp.com/line/link?linkToken=#{ token['linkToken'] }"
+              }
+            ]
+          }
+        }
       end
 
       def require_link_token(line_id)
