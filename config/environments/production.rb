791579abc1f0776e6a4663bac3413f1166a1b441
sou7322 <skoike322@gmail.com>
2021-10-13 17:54:07 +0900

    [Fix] 開発環境のログレベルを一時的にdebugに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,10 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :warn
+
+  # TODO: LINE機能のデバックのため一時的にレベルをdebugに
+  # TODO: 終了後warnに戻すこと
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
