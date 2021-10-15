9de781b70f9bba1282b88f4d1f1af849c6ec2bc0
sou7322 <skoike322@gmail.com>
2021-10-15 12:09:31 +0900

    [Fix] 連携ログイン時にuser.idとlinkTokenを合わせて取得する処理を一旦確定

--- app/controllers/line/authentications_controller.rb
+++ app/controllers/line/authentications_controller.rb
@@ -5,18 +5,16 @@ module Line
     layout 'line/layouts/line_login'
 
     def link
-      token_params
-      puts "linkToken#link: #{ @link_token }"
+      @link_token = params[:linkToken]
     end
 
     def create
       # 3. 自社サービスのユーザーIDを取得する
-      @user = login(params[:email], params[:password])
+      user = login(params[:email], params[:password])
+      link_token = params[:link_token]
 
-      if @user
+      if user
         user_id = current_user.id
-        # TODO: デバッグ用、忘れず削除
-        puts "linkToken#create: #{ @link_token }"
 
         # nonce生成
         # { nonce: user_id }で保存（nonce用のカラムを追加？連携が完了したら削除する）
@@ -24,17 +22,12 @@ module Line
         # 成功したらアカウント連携イベントが帰ってくる
         # linebotコントローラーへ
 
-        # redirect_to "https://i-hate-to-eat.herokuapp.com/register"
+        # TODO: リダイレクト確認用に一時的に設置
+        redirect_to "https://i-hate-to-eat.herokuapp.com/register"
       else
         flash.now[:danger] = "ログイン失敗"
         render :link
       end
     end
-
-    private
-
-      def token_params
-        @link_token = params[:linkToken]
-      end
   end
 end
