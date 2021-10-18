9c998def23bf7cdb645cb793dd1b268daff98c14
sou7322 <skoike322@gmail.com>
2021-10-18 23:16:22 +0900

    [Fix] 本番環境のログレベルをwarnに戻した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,9 +51,9 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
 
-  # TODO: LINE機能のデバックのため一時的にレベルをdebugに
+  # TODO: LINE機能のデバック時にのみレベルをdebugに
   # TODO: 終了後warnに戻すこと
-  config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
