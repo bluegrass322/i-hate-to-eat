bc861d1ba828276933c511176bbdafa09eec3d16
sou7322 <skoike322@gmail.com>
2021-10-21 19:12:01 +0900

    [Add] foodsコントローラーにshowアクションを実装

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -2,6 +2,10 @@ module Api
   module V1
     class FoodsController < Api::V1::BaseController
       def show
+        food = Food.find_by(id: params[:id])
+        json_string = FoodSerializer.new(food).serialized_json
+
+        render json: json_string
       end
     end
   end
