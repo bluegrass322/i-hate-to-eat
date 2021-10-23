ac835905183b4507aa00bda3b8f276b42c049e12
sou7322 <skoike322@gmail.com>
2021-10-23 23:50:34 +0900

    [Add] user_accountsコントローラーを生成

---
+++ app/controllers/api/v1/user_accounts_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UserAccountsController < Api::V1::BaseController
+      def show
+      end
+
+      def update
+      end
+    end
+  end
+end
