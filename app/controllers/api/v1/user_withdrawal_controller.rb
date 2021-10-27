b15cc63e26fa0712fd52e89cbba79f8b8effe7e7
sou7322 <skoike322@gmail.com>
2021-10-27 14:40:05 +0900

    [Add] withdrawalコントローラーにshow, destroyアクションを実装

--- app/controllers/api/v1/user_withdrawal_controller.rb
+++ app/controllers/api/v1/user_withdrawal_controller.rb
@@ -2,9 +2,13 @@ module Api
   module V1
     class UserWithdrawalController < Api::V1::BaseController
       def show
+        name = current_user.name
+        render json: { name: name }
       end
 
       def destroy
+        current_user.destroy!
+        head :ok
       end
     end
   end
