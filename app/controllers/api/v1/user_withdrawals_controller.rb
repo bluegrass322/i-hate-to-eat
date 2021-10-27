eaf1801c14fef42309eb81681ca7bbb1af3ca50f
sou7322 <skoike322@gmail.com>
2021-10-27 17:16:34 +0900

    [Fix] withdrawalコントローラーのファイル名を修正

--- app/controllers/api/v1/user_withdrawals_controller.rb
+++ app/controllers/api/v1/user_withdrawals_controller.rb
@@ -1,13 +1,16 @@
 module Api
   module V1
-    class UserWithdrawalController < Api::V1::BaseController
+    class UserWithdrawalsController < Api::V1::BaseController
       def show
+        id = current_user.id
         name = current_user.name
-        render json: { name: name }
+
+        render json: { id: id, name: name }
       end
 
       def destroy
         current_user.destroy!
+        # セッションの削除
         head :ok
       end
     end
