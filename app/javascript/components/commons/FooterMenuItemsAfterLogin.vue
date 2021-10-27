7f567adbab55ade30afd7b14cec668f772e3a7c1
sou7322 <skoike322@gmail.com>
2021-10-27 22:49:56 +0900

    [Fix] リントエラーを修正

--- app/javascript/components/commons/FooterMenuItemsAfterLogin.vue
+++ app/javascript/components/commons/FooterMenuItemsAfterLogin.vue
@@ -64,7 +64,10 @@ export default {
           });
 
           this.$router.push({ name: 'TopPage' });
-          this.$router.go({path: this.$router.currentRoute.path, force: true})
+          this.$router.go({
+            path: this.$router.currentRoute.path,
+            force: true,
+          });
         })
         .catch((error) => {
           let e = error.response;
