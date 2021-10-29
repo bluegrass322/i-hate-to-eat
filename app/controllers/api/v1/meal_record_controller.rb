582caec38b6e71a84877bba028c7aa3a39b339cf
sou7322 <skoike322@gmail.com>
2021-10-29 17:25:22 +0900

    [Add] meal_recordsコントローラーを生成

---
+++ app/controllers/api/v1/meal_record_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class MealRecordController < Api::V1::BaseController
+      def index
+      end
+
+      def show
+      end
+
+      def create
+      end
+    end
+  end
+end
