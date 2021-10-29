c0b06a60b422f46743dd485311aa990a45d5ffef
sou7322 <skoike322@gmail.com>
2021-10-29 16:07:20 +0900

    [Fix] eaten_foodsテーブル生成のマイグレーションファイルのタイポを修正

--- db/migrate/20211029052649_create_eaten_foods.rb
+++ db/migrate/20211029052649_create_eaten_foods.rb
@@ -1,8 +1,8 @@
 class CreateEatenFoods < ActiveRecord::Migration[6.0]
   def change
     create_table :eaten_foods do |t|
-      t.references :meal_records, foreign_key: true, null:false
-      t.references :foods, foreign_key: true, null:false
+      t.references :meal_record, foreign_key: true, null:false
+      t.references :food, foreign_key: true, null:false
 
       t.float :amount, null: false, default: 1.0
 
