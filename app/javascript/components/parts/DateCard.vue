cfca00fc8e1335a92c75fcd724c92c98e0de7dbc
sou7322 <skoike322@gmail.com>
2021-11-07 17:45:09 +0900

    [Add] 日付の表示を独立したコンポーネントに切り出し

---
+++ app/javascript/components/parts/DateCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-row>
+    <v-col cols="12" class="d-block date pa-0 pl-8">
+      <span class="text-h5 accent--text pa-0">
+        {{ date }}
+      </span>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+export default {
+  data() {
+    return {
+      date: null,
+    };
+  },
+  mounted() {
+    this.setDate();
+  },
+  methods: {
+    setDate() {
+      dayjs.extend(utc);
+      dayjs.extend(timezone);
+
+      const today = dayjs();
+      this.date = today.tz('Asia/Tokyo').format('YYYY.MM.DD');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.date {
+  border-bottom: 1px solid #f5f5f6;
+}
+</style>
