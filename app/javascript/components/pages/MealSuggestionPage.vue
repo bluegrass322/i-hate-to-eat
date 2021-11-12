a53b560bc9de72c261a4fefbf3f936c2b147e82f
sou7322 <skoike322@gmail.com>
2021-11-12 21:58:49 +0900

    [Fix] MealSuggestionPageのデザインを修正

--- app/javascript/components/pages/MealSuggestionPage.vue
+++ app/javascript/components/pages/MealSuggestionPage.vue
@@ -1,102 +1,90 @@
 <template>
-  <v-container class="container">
-    <v-row v-if="suggestionsExists" class="suggestion-row">
-      <v-col cols="12" md="6" class="pa-0">
-        <div class="suggestion-item">
-          <div class="base--text suggestion-items-title">
-            <span class="suggestion-items-title-text">Today's meal</span>
+  <div id="suggestions-page" class="mx-9 my-9 pa-0">
+    <v-row class="text-body-1 accent--text">
+      <!-- 食材一覧 ここから -->
+      <v-col cols="12" md="6" class="suggestions-item pa-0">
+        <div class="cutting-line font-weight-bold pt-3 pb-2 pl-4">
+          本日の食材
+        </div>
+
+        <div v-if="suggestionsExists" class="suggestion-item">
+          <food-card :suggestions="suggestions" />
+          <div class="arrow-text text-subtitle-2 my-7">
+            食べてみますか？
           </div>
-          <div class="meal-menus">
-            <template v-for="f in suggestions">
-              <v-sheet :key="f.id" color="base" class="foods-card">
-                <v-card
-                  flat
-                  tile
-                  color="primary"
-                  width="120"
-                  class="foods-card-contents"
-                >
-                  <v-card-text class="pa-0 text-caption text-center base--text">
-                    <div class="font-weight-medium">{{ f.name }}</div>
-                    <div>{{ f.subname }}</div>
-                    <div>{{ f.reference_amount * 100 }}g</div>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn
-                      color="base"
-                      dark
-                      text
-                      outlined
-                      small
-                      @click.stop="
-                        setFoodDetails(f.food_category_id, f.id);
-                        showDetail = true;
-                      "
-                    >
-                      詳細
-                    </v-btn>
-                    <v-dialog v-model="showDetail" scrollable>
-                      <food-detail />
-                    </v-dialog>
-                  </v-card-actions>
-                </v-card>
-              </v-sheet>
-            </template>
-            <div>
-              <v-btn
-                color="base"
-                dark
-                text
-                outlined
-                small
-                @click.stop="createMealRecord()"
-              >
-                食べる
-              </v-btn>
-              <v-btn
-                color="base"
-                dark
-                text
-                outlined
-                small
-                @click.stop="destroySuggestions()"
-              >
-                食べない
-              </v-btn>
-            </div>
+          <div class="d-flex flex-wrap justify-center align-center">
+            <v-btn
+              color="accent"
+              dark
+              :height="btnHeihgt"
+              small
+              tile
+              outlined
+              :width="btnWidth"
+              class="confirm-btn"
+              @click.stop="destroySuggestions()"
+            >
+              キャンセル
+            </v-btn>
+            <v-btn
+              color="accent"
+              dark
+              :height="btnHeihgt"
+              small
+              tile
+              outlined
+              :width="btnWidth"
+              class="confirm-btn"
+              @click.stop="createMealRecord()"
+            >
+              食べる
+            </v-btn>
           </div>
         </div>
-      </v-col>
-      <v-col cols="12" md="6" class="pa-0">
-        <div>
-          <nutrients-achievement />
+        <div v-else class="suggestions-placeholder d-flex justify-center align-center text-body-2 accent--text">
+          <div>{{ noneMessage }}</div>
         </div>
       </v-col>
+      <!-- 食材一覧 ここまで -->
+
+      <!-- 栄養素合計 ここから -->
+      <v-col cols="12" md="6" class="suggestions-item mt-7 mt-md-0 pa-0">
+        <div class="cutting-line font-weight-bold pt-3 pb-2 pl-4">
+          摂取できる栄養
+        </div>
+        <nutrients-achievement />
+      </v-col>
+      <!-- 栄養素合計 ここまで -->
     </v-row>
-    <v-row v-else class="none-message">
-      <div class="none-menus base--text">
-        <span>{{ noneMessage }}</span>
-      </div>
-    </v-row>
-  </v-container>
+  </div>
 </template>
 
 <script>
-import foodDetail from '../parts/FoodDetail';
+import FoodCard from '../parts/FoodCard';
 import NutrientsAchievement from '../parts/NutrientsAchievement';
 
 export default {
   components: {
-    foodDetail,
+    FoodCard,
     NutrientsAchievement,
   },
   data() {
     return {
-      suggestions: {},
+      suggestions: null,
       suggestionsExists: false,
-      showDetail: false,
-      noneMessage: '本日の食事メニューは存在しません',
+      noneMessage: '存在しません',
+      btnHeihgt: '45',
     };
+  },
+  computed: {
+    btnWidth() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          return '100%';
+        default:
+          return '50%';
+      }
+    },
   },
   mounted() {
     this.setSuggestions();
@@ -148,20 +136,6 @@ export default {
           console.error(e.response.status);
         });
     },
-    setFoodDetails(categoryId, foodId) {
-      this.axios
-        .get(`/api/v1/food_categories/${categoryId}/foods/${foodId}`)
-        .then((res) => {
-          console.log(res.status);
-          this.$store.dispatch(
-            'foodDetails/setAttributes',
-            res.data.data.attributes
-          );
-        })
-        .catch((e) => {
-          console.error(e.response.status);
-        });
-    },
     initializeSuggestions() {
       this.suggestions = {};
       this.suggestionsExists = false;
@@ -171,28 +145,25 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  margin: 0;
-  padding: 0;
+.v-btn.confirm-btn {
+  color: white;
+  background-color: rgba(90, 120, 153, 0.3);
+  border: 1.5px solid #f5f5f6;
+  box-sizing: border-box;
+  text-transform: none !important;
 }
 
-.none-message {
-  margin: 30px 20px;
+#suggestions-page {
+  width: 100%;
 }
 
-.suggestion-row {
-  background-color: #5a7899;
-  margin: 30px 20px;
+.arrow-text {
+  text-align: center;
+  vertical-align: middle;
 }
 
-.suggestion-item {
-  margin-bottom: 40px;
-}
-
-.suggestion-items-title {
-  border-bottom: 1px solid #f5f5f6;
-  height: 48px;
-  margin-bottom: 15px;
+.cutting-line {
+  border-bottom: 1px dashed rgba(245, 245, 246, 1);
 }
 
 .meal-menus {
@@ -201,12 +172,16 @@ export default {
   justify-content: center;
 }
 
-.foods-card {
-  border: 1px solid #f5f5f6;
-}
-
 .foods-card-contents {
   padding: 8px 8px;
+}
+
+.suggestions-item {
+  border: 2px solid rgba(245, 245, 246, 1);
+}
+
+.suggestions-placeholder {
+  height: 150px;
 }
 
 .tabs {
