dac8ecf9c2a8c11e58607b504c2ce9f78a8b33e4
sou7322 <skoike322@gmail.com>
2021-11-14 21:14:14 +0900

    [Fix] マイページ共通のトップリンクのデザインを修正

--- app/javascript/components/pages/MyPage.vue
+++ app/javascript/components/pages/MyPage.vue
@@ -1,44 +1,35 @@
 <template>
   <div id="mypage" class="ma-0 pa-0">
-    <v-row class="header-btns">
-      <v-col cols="12" class="d-flex justify-center">
-        <v-btn
-          to="/mypage/setting"
-          color="accent"
-          text
-          tile
-          :height="btnHeight"
-          :width="btnWidth"
-          class="footer-btn ma-0 px-2 flex-grow-1 text-caption text-sm-suubtitle-1"
-        >
-          栄養摂取基準設定
-        </v-btn>
-        <v-btn
-          to="/mypage/account"
-          color="accent"
-          text
-          tile
-          :height="btnHeight"
-          :width="btnWidth"
-          class="footer-btn ma-0 px-2 flex-grow-1 text-caption text-sm-suubtitle-1"
-        >
-          アカウント設定
-        </v-btn>
-        <v-btn
-          to="/mypage/withdrawal"
-          color="accent"
-          text
-          tile
-          :height="btnHeight"
-          :width="btnWidthS"
-          class="footer-btn ma-0 px-2 flex-grow-1 text-caption text-sm-suubtitle-1"
-        >
-          退会
-        </v-btn>
+    <v-row class="top-row mt-6 mb-5">
+      <v-col cols="12" class="ma-0 pa-0">
+        <div class="top-links d-flex flex-column align-end">
+          <router-link
+            to="/mypage/setting"
+            class="link-item d-flex align-end text-body-2 accent--text mb-3"
+          >
+            栄養摂取基準設定
+            <div class="under-line line-first" />
+          </router-link>
+          <router-link
+            to="/mypage/account"
+            class="link-item d-flex align-end text-body-2 accent--text mb-2"
+          >
+            アカウント設定
+            <div class="under-line line-second" />
+          </router-link>
+          <router-link
+            to="/mypage/withdrawal"
+            class="link-item d-flex align-end text-body-2 accent--text"
+          >
+            退会
+            <div class="under-line line-third" />
+          </router-link>
+        </div>
       </v-col>
     </v-row>
+
     <v-row>
-      <v-col>
+      <v-col class="mypage-content px-6">
         <router-view />
       </v-col>
     </v-row>
@@ -57,22 +48,48 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 #mypage {
   width: 100%;
 }
 
-.v-btn.footer-btn {
-  align-items: center;
-  justify-content: center;
+.link-item {
+  text-decoration: none;
 }
 
-.v-list-item.small {
-  min-height: 25px !important;
+.under-line {
+  border-bottom: 1px solid rgb(245, 245, 246);
+  margin-left: 5px;
+  position: relative;
+  top: -6px;
 }
 
-.footer-btn {
-  /* info */
-  border-right: 3px solid rgba(137, 167, 202, 1);
+.line-first {
+  width: 90px;
 }
+
+.line-second {
+  width: 50px;
+}
+
+.line-third {
+  width: 210px;
+}
+
+.top-row {
+  position: fixed;
+  top: 0%;
+  right: 0;
+}
+
+.mypage-content {
+  margin: 120px 36px 64px 36px;
+}
+
+
+// .top-row {
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+// }
 </style>
