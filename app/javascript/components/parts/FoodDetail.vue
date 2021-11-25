42630e3db6172a6075625b4d1176deea05e93d77
sou7322 <skoike322@gmail.com>
2021-11-26 00:16:44 +0900

    [Fix] FoodDetailからv-sheetを削除

--- app/javascript/components/parts/FoodDetail.vue
+++ app/javascript/components/parts/FoodDetail.vue
@@ -1,62 +1,69 @@
 <template>
-    <v-card color="back" flat tile max-height="500" class="detail-card ma-0 pa-0">
-      <v-card-title class="d-flex align-end accent--text my-3">
-        <div class="d-flex">
-          <div class="text-subtitle-2">{{ basicInfo.name }}</div>
-          <div class="text-caption font-weight-midium ml-1">
-            {{ basicInfo.subname }}
+  <v-card color="back" flat tile max-height="500" class="detail-card ma-0 pa-0">
+    <v-card-title class="d-flex align-end accent--text my-3">
+      <div class="d-flex">
+        <div class="text-subtitle-2">{{ basicInfo.name }}</div>
+        <div class="text-caption font-weight-midium ml-1">
+          {{ basicInfo.subname }}
+        </div>
+      </div>
+      <div class="text-body-2 ml-2">
+        {{ basicInfo.reference_amount * 100 }}g
+      </div>
+    </v-card-title>
+
+    <v-card-text class="ma-0 pa-0">
+      <v-tabs
+        background-color="pDark"
+        color="base"
+        dark
+        center-active
+        grow
+        class="ma-0 pa-0"
+      >
+        <v-tab class="tabs">Macro</v-tab>
+        <v-tab class="tabs">Vitamin</v-tab>
+        <v-tab class="tabs-end">Mineral</v-tab>
+
+        <v-tab-item class="tab-items ma-0 pa-0">
+          <div class="d-flex flex-wrap align-center ma-0 pa-0">
+            <template v-for="m in macro">
+              <nutrients-card
+                :key="m.label"
+                :label="m.label"
+                :amount="m.amount"
+                :unit="m.unit"
+              />
+            </template>
           </div>
-        </div>
-        <div class="text-body-2 ml-2">
-          {{ basicInfo.reference_amount * 100 }}g
-        </div>
-      </v-card-title>
-
-      <v-card-text class="ma-0 pa-0">
-        <v-tabs background-color="pDark" color="base" dark center-active grow class="ma-0 pa-0">
-          <v-tab class="tabs">Macro</v-tab>
-          <v-tab class="tabs">Vitamin</v-tab>
-          <v-tab class="tabs-end">Mineral</v-tab>
-
-          <v-tab-item class="tab-items ma-0 pa-0">
-            <div class="d-flex flex-wrap align-center ma-0 pa-0">
-              <template v-for="m in macro">
-                <nutrients-card
-                  :key="m.label"
-                  :label="m.label"
-                  :amount="m.amount"
-                  :unit="m.unit"
-                />
-              </template>
-            </div>
-          </v-tab-item>
-          <v-tab-item class="tab-items">
-            <div class="d-flex flex-wrap align-center">
-              <template v-for="v in vitamins">
-                <nutrients-card
-                  :key="v.label"
-                  :label="v.label"
-                  :amount="v.amount"
-                  :unit="v.unit"
-                />
-              </template>
-            </div>
-          </v-tab-item>
-          <v-tab-item class="tab-items">
-            <div class="d-flex flex-wrap align-center">
-              <template v-for="m in minerals">
-                <nutrients-card
-                  :key="m.label"
-                  :label="m.label"
-                  :amount="m.amount"
-                  :unit="m.unit"
-                />
-              </template>
-            </div>
-          </v-tab-item>
-        </v-tabs>
-      </v-card-text>
-    </v-card>
+        </v-tab-item>
+        <v-tab-item class="tab-items">
+          <div class="d-flex flex-wrap align-center">
+            <template v-for="v in vitamins">
+              <nutrients-card
+                :key="v.label"
+                :label="v.label"
+                :amount="v.amount"
+                :unit="v.unit"
+              />
+            </template>
+          </div>
+        </v-tab-item>
+        <v-tab-item class="tab-items">
+          <div class="d-flex flex-wrap align-center">
+            <template v-for="m in minerals">
+              <nutrients-card
+                :key="m.label"
+                :label="m.label"
+                :amount="m.amount"
+                :unit="m.unit"
+              />
+            </template>
+          </div>
+        </v-tab-item>
+      </v-tabs>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
