e758433418896a335bf917c3a65783b16d182c56
sou7322 <skoike322@gmail.com>
2021-11-25 11:41:45 +0900

    [Fix] destroy_suggestions_allメソッドにデバッグ用の記述を追加

--- app/controllers/concerns/suggestions_destroyable.rb
+++ app/controllers/concerns/suggestions_destroyable.rb
@@ -4,13 +4,25 @@ module SuggestionsDestroyable
   extend ActiveSupport::Concern
 
   def destroy_suggestions_all(user)
+    # 要削除
+    Rails.logger.debug "destroy_suggestions_allメソッドに突入"
+
     suggestions = user.suggestions.where(target_date: Time.zone.today)
+
+    # 要削除
+    Rails.logger.debug "suggestions.blank?: #{suggestions.blank?}"
+    Rails.logger.debug "suggestions: #{suggestions}"
+
     return render404(nil, "Suggestionが存在しません") if suggestions.blank?
 
     begin
+      # 要削除
+      Rails.logger.debug "トランザクションに突入"
+
       Suggestion.transaction do
         suggestions.each(&:destroy!)
       end
+      Rails.logger.debug "トランザクションが終了"
     rescue StandardError => e
       # TODO: 例外処理を修正
       Rails.logger.warn "User#{user.id}: Failed to create meal record. Cause...'#{e}'"
