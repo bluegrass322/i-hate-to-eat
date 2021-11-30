f0b0f3251f9e33a58ec2120a336de8e2ddf0e815
sou7322 <skoike322@gmail.com>
2021-12-01 00:03:39 +0900

    [Fix] current_userを不要にインスタンス変数に代入していた処理を削除

--- app/controllers/api/v1/bmrs_controller.rb
+++ app/controllers/api/v1/bmrs_controller.rb
@@ -3,24 +3,18 @@
 module Api
   module V1
     class BmrsController < Api::V1::BaseController
-      before_action :set_user
-
       def update
-        @user.assign_attributes(bmr_params)
-        @user.bmr = @user.calc_bmr.floor
+        current_user.assign_attributes(bmr_params)
+        current_user.bmr = current_user.calc_bmr.floor
 
-        if @user.save
-          render json: { bmr: @user.bmr, pfc_params: @user.set_attributes_for_pfc }
+        if current_user.save
+          render json: { bmr: current_user.bmr, pfc_params: current_user.set_attributes_for_pfc }
         else
-          render400(nil, @user.errors.full_messages)
+          render400(nil, current_user.errors.full_messages)
         end
       end
 
       private
-
-        def set_user
-          @user = User.find(current_user.id)
-        end
 
         def bmr_params
           params.require(:user).permit(:gender, :birth, :height, :weight)
