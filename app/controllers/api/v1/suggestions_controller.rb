ed3ede1841a6206625d08929e3da392fcce5a009
sou7322 <skoike322@gmail.com>
2021-12-01 16:23:33 +0900

    [Fix] suggestions_controllerをリファクタリング

--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -3,6 +3,7 @@
 module Api
   module V1
     class SuggestionsController < Api::V1::BaseController
+      include TotalAndAchvGetable
       include SuggestionsCreatable
       include SuggestionsDestroyable
 
@@ -28,15 +29,15 @@ module Api
       end
 
       def update
-        Suggestion.transaction do
-          destroy_suggestions_all(current_user) if current_user.suggestions.present?
-          meals = create_suggestions(current_user)
-          response = set_suggestions_response(current_user, meals)
+        destroy_suggestions_all(current_user) if current_user.suggestions.present?
+        meals = create_suggestions(current_user)
+        response = set_suggestions_response(current_user, meals)
 
+        if response.present?
           render json: response
+        else
+          render500(nil, "食事内容のリロードに失敗しました")
         end
-      rescue StandardError
-        render500(nil, "食事内容のリロードに失敗しました") and return
       end
 
       private
@@ -45,21 +46,9 @@ module Api
           render500(nil, "本日は既に食事をとっています") if current_user.meal_records.for_today.present?
         end
 
-        def get_intake_total(foods)
-          total = NutritionTotal.new
-          total.calc_intake_total(foods)
-        end
-
-        def get_achievement(total, bmr, pfc, dri)
-          achv = IntakeAchievement.new
-          achv.calc_intake_achievement(total, bmr, pfc, dri)
-        end
-
         def set_suggestions_response(user, meals)
-          amt_pfc = user.set_attributes_for_pfc[:amt]
-
           total = get_intake_total(meals)
-          achv = get_achievement(total, user.bmr, amt_pfc, user.dietary_reference_intake)
+          achv = get_achievement(user, total)
 
           { meals: meals, total: total, achv: achv }
         end
