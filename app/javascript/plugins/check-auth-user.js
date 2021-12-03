87e67d7b2ef5ca814def1e82989e9c39c51880e8
sou7322 <skoike322@gmail.com>
2021-12-03 16:16:49 +0900

    [Fix] check-auth-user.jsをリファクタリング

--- app/javascript/plugins/check-auth-user.js
+++ app/javascript/plugins/check-auth-user.js
@@ -2,10 +2,10 @@ import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 
-export const checkAuthUserExpires = (expires) => {
-  dayjs.extend(utc);
-  dayjs.extend(timezone);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
+export const checkAuthUserExpires = (expires) => {
   const today = dayjs().tz('Asia/Tokyo');
   const expiresAt = dayjs(expires).tz('Asia/Tokyo');
 
