d10753fe6c804e8a82095d8b3440c021f4288e67
sou7322 <skoike322@gmail.com>
2021-12-10 12:01:50 +0900

    [Fix] notificatable_to_adminのメソッド名を見直し

--- app/controllers/concerns/notificatable_to_admin.rb
+++ app/controllers/concerns/notificatable_to_admin.rb
@@ -5,13 +5,13 @@ module NotificatableToAdmin
   extend ActiveSupport::Concern
 
   def notice_to_admin(text)
-    message = set_message(text)
+    message = make_message(text)
     client.push_message(admin_line_id, message)
   end
 
   def complete_message(task_name)
     text = "#{task_name}が正常に終了"
-    message = set_message(text)
+    message = make_message(text)
 
     client.push_message(admin_line_id, message)
   end
@@ -29,7 +29,7 @@ module NotificatableToAdmin
       end
     end
 
-    def set_message(text)
+    def make_message(text)
       date_time = Time.current.strftime("%c")
       { type: "text", text: "#{date_time}\n#{text}" }
     end
