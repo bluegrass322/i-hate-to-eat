b0175ab0100e88172c31e26eaeaaacca318a0a1a
sou7322 <skoike322@gmail.com>
2021-12-10 23:47:28 +0900

    [Add] BMRとPFCのg数を返却するメソッドを切り出し

---
+++ app/lib/linebot/bmr_and_pfc_replier.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class BmrAndPfcReplier < LinebotBase
+  def self.call(user)
+    new(user).call
+  end
+
+  def call
+    amount = bmr_and_pfc_amount
+    text = textualize(amount)
+
+    reply_text(text)
+  end
+
+  private
+
+    def initialize(user)
+      @user = user
+    end
+
+    def bmr_and_pfc_amount
+      @user.attributes_pfc[:amt]
+    end
+
+    def textualize(amount)
+      "#{@user.name}さん \n\n" +
+      "BMR: #{@user.bmr}kcal \n" +
+      "P: #{amount[:protein]}g \n" +
+      "F: #{amount[:fat]}g \n" +
+      "C: #{amount[:carbohydrate]}g"
+    end
+end
