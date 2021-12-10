29845419ed82d77b9b990afcc7dd548d6f163815
sou7322 <skoike322@gmail.com>
2021-12-11 00:11:20 +0900

    [Add] 食事の提案内容を返却するメソッドを切り出し

---
+++ app/lib/linebot/suggestions_replier.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class SuggestionsReplier < LinebotBase
+  def self.call(user)
+    new(user).call
+  end
+
+  def call
+    text = @user.make_meal_menu_for_line
+    reply_text(text)
+  end
+
+  private
+
+    def initialize(user)
+      @user = user
+    end
+end
