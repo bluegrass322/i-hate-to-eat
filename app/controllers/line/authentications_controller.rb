75018c41c6c670959066de32d1aa2bfee67d9ae9
sou7322 <skoike322@gmail.com>
2021-12-11 21:41:57 +0900

    [Fix] デバッグ用ロギング処理のタイポを修正

--- app/controllers/line/authentications_controller.rb
+++ app/controllers/line/authentications_controller.rb
@@ -11,22 +11,20 @@ module Line
     def create
       # 連携手順3. 自社サービスのユーザーIDを取得する
       login_user = login(params[:name], params[:password])
-      Rails.logger.debug "User: #{login_user}"
 
       if login_user
         link_token = params[:link_token]
-        Rails.logger.debug "LinkToken: #{link_token}"
  
         # 連携手順4. nonceを生成してユーザーをLINEプラットフォームにリダイレクトする
         nonce = SecureRandom.urlsafe_base64(16)
-        Rails.logger.debug "Nonce: #{nonce}"
 
         begin
+          binding.pry
           login_user.update!(line_nonce: nonce)
         rescue => e
           Rails.logger.error "#{e}"
         end
-        Rails.logger.debug "UserNonce: #{login_user.nonce}"
+        Rails.logger.debug "UserNonce: #{login_user.line_nonce}"
 
         # uri = URI("https://access.line.me/dialog/bot/accountLink")
         # uri.query = URI.encode_www_form({ linkToken: link_token, nonce: nonce })
