a25b80c9bf52ce71bd6f1b9526229e798f1ef968
sou7322 <skoike322@gmail.com>
2021-12-11 23:16:20 +0900

    [Add] アカウント連携解除処理を切り出し

---
+++ app/lib/linebot/account_linking_remover.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require './app/lib/linebot/linebot_base'
+
+class AccountLinkingRemover < LinebotBase
+  def self.call(user)
+    new(user).call
+  end
+
+  def call
+    remove_linking
+    reply_text("LINEアカウントの連携を解除しました")
+  end
+
+  private
+
+    def initialize(user)
+      @user = user
+    end
+
+    def remove_linking
+      @user.update!(line_user_id: nil)
+    rescue StandardError => e
+      Rails.logger.error "User#{@user.id}: Failed to remove account linking. Cause...'#{e}'"
+      notice_to_admin("LINEアカウント連携解除に失敗")
+    end
+end
