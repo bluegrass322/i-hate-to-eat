427b9f0e7ec971005f7a3ddddf49123165cf547c
sou7322 <skoike322@gmail.com>
2021-12-13 14:35:17 +0900

    [Fix] 自作クラスのディレクトリ構成変更にともないrequire文を修正

--- app/lib/classes/line/nonce_and_redirect_uri_creater.rb
+++ app/lib/classes/line/nonce_and_redirect_uri_creater.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require './app/lib/linebot/linebot_base'
+require './app/lib/classes/line/linebot_base'
 
 class NonceAndRedirectUriCreater < LinebotBase
   def self.call(user, link_token)
--- app/lib/classes/line/suggestions_replier.rb
+++ app/lib/classes/line/suggestions_replier.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require './app/lib/linebot/linebot_base'
+require './app/lib/classes/line/linebot_base'
 
 class SuggestionsReplier < LinebotBase
   def self.call(user)
