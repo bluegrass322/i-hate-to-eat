81a2a0d72b0f383d8e87e374b07bbe3291b7fa17
sou7322 <skoike322@gmail.com>
2021-12-20 21:46:06 +0900

    [Fix] SignupAndLoginMacrosを一部変更

--- spec/support/api/signup_and_login_macros.rb
+++ spec/support/api/signup_and_login_macros.rb
@@ -1,8 +1,8 @@
 module SignupAndLoginMacros
   def signup_and_login
     create(:dietary_reference_intake)
-    user = create(:user)
+    @user = create(:user)
 
-    post api_v1_authentication_path, params: { name: user.name, password: "password" }
+    post api_v1_authentication_path, params: { name: @user.name, password: "password" }
   end
 end
