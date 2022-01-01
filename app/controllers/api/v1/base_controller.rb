49a1bc0b920bad1f8bad48542278bccb20df133c
sou7322 <skoike322@gmail.com>
2022-01-02 00:10:24 +0900

    [Fix] ExeptionHandlerのinclude記述を修正

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class BaseController < ApplicationController
-      include Api::ExceptionHandler
+      include ExceptionHandler
 
       before_action :require_login
 
--- app/controllers/line/base_controller.rb
+++ app/controllers/line/base_controller.rb
@@ -3,7 +3,7 @@
 module Line
   class BaseController < ApplicationController
     require 'line/bot'
-    include Api::ExceptionHandler
+    include ExceptionHandler
 
     before_action :validate_signature
 
