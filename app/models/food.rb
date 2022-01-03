508645be74cdaec76b0caa413ecbc0a1110b5559
sou7322 <skoike322@gmail.com>
2022-01-03 19:31:23 +0900

    [Fix] Foodモデルのスコープを修正
    Foodモデルのメインディッシュ・サイドディッシュを抽出するスコープについて、
    これまでは`food_category_id`を使用していたが、FoodCategoryのデータを削除して
    作り直した場合に不整合が起きてしまう。
    そこでFoodCategoryモデルに`name`を用いたスコープを追加し、それをFoodモデルで
    使用するように変更。

--- app/models/food.rb
+++ app/models/food.rb
@@ -11,12 +11,8 @@ class Food < ApplicationRecord
   scope :prio_m, -> { where(priority: 10) }
   scope :prio_r, -> { where(priority: 5) }
   scope :prio_rm, -> { where(priority: 5..10) }
-
-  # TODO: category_idでなくfood_categoryのnameを使う
-  # もしくは判別用のintegerカラムを追加する
-  # もしデータを入れ直した際にズレてしまう & テストが作りにくい
-  scope :maindish, -> { where(food_category_id: [10, 11]) }
-  scope :sidedish, -> { where(food_category_id: 1..9) }
+  scope :maindish, -> { joins(:food_category).merge(FoodCategory.main_cate) }
+  scope :sidedish, -> { joins(:food_category).merge(FoodCategory.side_cate) }
 
   # Validation
   with_options presence: true do
